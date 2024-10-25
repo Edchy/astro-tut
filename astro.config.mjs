1298cd471da746cba301a32144f8a83c09f45a67
edch <soderblomed@gmail.com>
2024-10-25 11:41:56 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-tuttt.netlify.app/"
+  site: "https://astro-tut-six.vercel.app/"
 });
